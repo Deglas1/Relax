2d3effcff6b8cc2963c14ab676bab7a50b79a3e8
Kuro-99 <78974541+Kuro-99@users.noreply.github.com>
2021-08-03 21:27:25 +0300

    Create pixe.js
    Новая команда в боте к обновлению 1.2.1

---
+++ commands/Other/pixe.js
@@ -0,0 +1,11 @@
+module.exports = ({
+
+name: "pixelate",
+
+code: `$title[Pixelated!]
+
+$color[$getvar[color]]
+
+$image[https://api.willz.repl.co/image/pixel?image=$useravatar[$finduser[$message]]`
+
+}) 
